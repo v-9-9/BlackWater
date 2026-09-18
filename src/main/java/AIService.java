33c92dda423def9c9fe091066b5abf74ca854de5
v-9-9 <ubx3.po@gmail.com>
2026-09-18 07:39:27 +0300

    Update AIService.java

--- src/main/java/AIService.java
+++ src/main/java/AIService.java
@@ -1,1 +1,9 @@
+import org.springframework.stereotype.Service;
 
+@Service
+public class AIService {
+
+    public String generate(String message) {
+        return "Blackwater received: " + message;
+    }
+}
