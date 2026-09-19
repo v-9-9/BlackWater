b365dc5a28313a7c04f9ca4ba61880ab852255e2
v-9-9 <ubx3.po@gmail.com>
2026-09-19 06:07:05 +0300

    Update BlackwaterApplication.java

--- src/main/java/BlackwaterApplication.java
+++ src/main/java/BlackwaterApplication.java
@@ -1,6 +1,8 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication(scanBasePackages = {
         "controller",
         "service"
@@ -8,6 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BlackwaterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BlackwaterApplication.class, args);
+        SpringApplication.run(
+                BlackwaterApplication.class,
+                args
+        );
     }
 }
