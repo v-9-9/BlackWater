4f305a925f89f523421b3069f698fd3bf67e08b0
v-9-9 <ubx3.po@gmail.com>
2026-09-20 14:37:19 +0300

    Update LearningController.java

--- src/main/java/controller/LearningController.java
+++ src/main/java/controller/LearningController.java
@@ -1,7 +1,10 @@
 package controller;
 
 import org.springframework.web.bind.annotation.*;
+import service.KnowledgeEntry;
 import service.LearningEngine;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/learn")
@@ -15,7 +18,7 @@ public class LearningController {
     }
 
     @PostMapping
-    public String learn(@RequestBody String topic) {
+    public List<KnowledgeEntry> learn(@RequestBody String topic) {
         return learningEngine.learn(topic);
     }
 }
